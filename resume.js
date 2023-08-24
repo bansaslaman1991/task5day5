f8e98f789a2b1694bac64864cfeda1ebcffd4428
Aman Bansal <133233860+bansaslaman1991@users.noreply.github.com>
2023-08-24 20:40:06 +0530

    file uploaded

---
+++ resume.js
@@ -0,0 +1,10 @@
+var resume = {
+  name:"AMAN BANSAL"
+  Age:31
+  address:"B904, Probo Prolix Pride, Ravet, Pune-412101"
+  emailadd:"bansalaman1991@gmail.com"
+  degree:"12th pass"
+  course:"Full Stack Development"
+  institute:"Guvi"
+  experience:"fresher"
+}
